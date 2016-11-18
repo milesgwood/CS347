3f5a366ad176cd130a2d5158256981a5e5d5e35c
milesgwood <milesgwood@gmail.com>
2016-11-17 19:52:57 -0500

    Adding a SQLTest to see how to access .sql files on path

---
+++ JMU-Share/test/SQLTests.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author greatwmc
+ */
+public class SQLTests {
+
+    public SQLTests() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void testSQLParser() {
+        assertEquals("CREATE TABLE comments IF NOT EXISTS (id INTEGER PRIMARY KEY, author_id INTEGER, post_ID INTEGER, comment VARCHAR(255));", sql.SQLParser.sqlStringCreation("create_tables.sql"));
+    }
+}
